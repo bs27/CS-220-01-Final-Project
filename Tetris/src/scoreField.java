cdbdd51510eff52db4a6ab31078ba92f937a1d89
bs27 <bsottile@callutheran.edu>
2022-05-24 12:48:07 -0700

    Upload to Github
    Upload

---
+++ Tetris/src/scoreField.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+
+public class scoreField extends JTextField {
+    public int Score;
+    scoreField(){
+        super("0",5);
+        Score = 0;
+        this.setEditable(false);
+    }
+    public void setScore(int newScore){
+        Score = newScore;
+        this.setEditable(true);
+        this.setText(Integer.toString(newScore));
+        this.setEditable(false);
+    }
+    public void incrementScore(){
+        Score++;
+        this.setEditable(true);
+        this.setText(Integer.toString(Score));
+        this.setEditable(false);
+    }
+
+    public int getScore() {
+        return Score;
+    }
+}
